e3abfe132c6273a8731677d94a1e14fc1cb1a904
Syaiful <90619899+Kuma1227@users.noreply.github.com>
2022-11-17 07:52:20 +0700

    komit pertama

---
+++ array&contohperulangan.py
@@ -0,0 +1,27 @@
+#nilai = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
+#for i in nilai:
+#    print(nilai[i])
+
+#hari = ['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu', 'minggu']
+#for index, value in enumerate (hari):
+#    print (index)
+#    print(value)
+
+
+#listnama = []
+
+#for i in range (3):
+#    nama = str(input('masukan nama : '))
+#    listnama.append(nama)
+
+#print(listnama)
+
+nama = ['gblk', 'ajg', 'mmk']
+jum = len (nama) - 1
+res = ""
+while jum >= 0:
+    res += nama [jum]
+    jum -= 1
+print (res)
+
+
